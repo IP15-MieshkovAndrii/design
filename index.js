341ed9e081fc00a0709d2b2389596259b7dde02d
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2023-09-12 20:09:45 +0200

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js
+// import '@material/web/button/text-button.js';
+// import '@material/web/button/outlined-button.js';
+// import '@material/web/chips/filter-chip.js';
+import '@material/web/all.js';
